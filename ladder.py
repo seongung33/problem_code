46ed9de6ea4bc55ec3e87aba6a2915d9f473df44
Seongung <seongung33@gmail.com>
2026-02-06 23:28:32 +0900

    20260206_last

--- ladder.py
+++ ladder.py
@@ -1,14 +1,14 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
 for test in range(1, 11):
     n = int(input())
     matrix = [list(map(int, input().split())) for _ in range(100)]
-    for i in range(100):
-        for j in range(100):
-            if matrix[i][j] == 2:
-                goal_y = i
-                goal_x = j
+    for j in range(100):
+        if matrix[99][j] == 2:
+            goal_x = j
     goal_x = goal_x
-    goal_y = goal_y
-
+    goal_y = 99
     # 동 북 서
     dx = [1, 0, -1]
     dy = [0, -1, 0]
@@ -17,21 +17,30 @@ for test in range(1, 11):
 
     def in_range(y, x):
         return 0 <= y < 100 and 0 <= x < 100
-    while True:
-        # 1. 앞으로 간다.
+    while goal_y != 0:
+        # 1. 북쪽으로 향하면서
         # 2. 오른쪽 혹은 왼쪽으로 갈 수 있는지 확인한다.
-        # 3. 갈 수 있다면 방향 전환을 한다.
-        # 4. 왼쪽으로 갔다면 진행 후 오른쪽으로 갈 수 있는지 확인한다.
-        # 5. 오른쪽으로 갔다면 진행 후 왼쪽으로 갈 수 있는지 확인한다.
-        # 6. 세 방향 모두 진행 불가능 하다면 이는 사다리의 시작지점이다.
+        # 3. 갈 수 있다면 해당 방향으로 간다.
+        # 4. 불가능 하다면 북쪽으로 간다.
+
+        # 5. 오른쪽 혹은 왼쪽으로 이동 시
+        # 6. 북쪽으로 갈 수 있는 지 확인한다.
+        # 7. 가능하다면 북쪽으로 진행하고 불가능 하다면 오른쪽 혹은 왼쪽으로 진행한다.
+        # 8. y값이 0이 되면 출발지점 도달이다.
+
 
         # 전진
-        ny = goal_y + dy[d]
-        nx = goal_x + dx[d]
+        # ny = goal_y + dy[d]
+        # nx = goal_x + dx[d]
+        # goal_y = ny
+        # goal_x = nx
 
-        #오른쪽, 혹은 왼쪽으로 갈 수 있다면
+        #전진중
         if d == 1:
-            for D in range(0,3,2):
+            # 오른쪽, 혹은 왼쪽으로 갈 수 있다면
+            # 오른쪽 혹은 왼쪽으로 이동한다.
+            s = 0
+            for D in 0, 2:
                 ny = goal_y + dy[D]
                 nx = goal_x + dx[D]
                 if in_range(ny, nx):
@@ -39,26 +48,48 @@ for test in range(1, 11):
                         d = D
                         goal_y = ny
                         goal_x = nx
+                        s += 1
+                        break
+            # 좌우로 이동을 못했을 경우 전진한다.
+            if s == 0:
+                ny = goal_y + dy[d]
+                nx = goal_x + dx[d]
+                if in_range(ny, nx):
+                    if matrix[ny][nx] == 1:
+                        goal_y = ny
+                        goal_x = nx
                         continue
+        # 오른쪽 or 왼쪽으로 진행중 북 쪽으로 갈 수 있을 경우
+        # 갈 수 있다면 북 쪽으로 간다.
         if d in (0, 2):
-            ny = goal_y + dy[d]
-            nx = goal_x + dx[d]
+            ny = goal_y + dy[1]
+            nx = goal_x + dx[1]
             if in_range(ny, nx):
                 if matrix[ny][nx] == 1:
                     d = 1
                     goal_y = ny
                     goal_x = nx
                     continue
-        if d == 1:
-            ny = goal_y + dy[d]
-            nx = goal_x + dx[d]
-            if not in_range(ny, nx):
-                goal_y = ny
-                goal_x = nx
-                break
+        #가지 못했을 경우
+        # 오른쪽 혹은 왼쪽으로 진행한다.
+                else:
+                    ny = goal_y + dy[d]
+                    nx = goal_x + dx[d]
+                    if in_range(ny, nx):
+                        if matrix[ny][nx] == 1:
+                            goal_x = nx
+                            goal_y = ny
+                            continue
+        # if d == 1:
+        #     ny = goal_y + dy[d]
+        #     nx = goal_x + dx[d]
+        #     if not in_range(ny, nx):
+        #         break
+        # print(goal_y, goal_x)
         # goal_y = ny
         # goal_x = nx
     print(f"#{test} {goal_x}")
+    # print(goal_y)
 
 
 
--- snail_climb.py
+++ snail_climb.py
@@ -1,28 +1,26 @@
-for test in range(1, 11):
-    n = int(input())
-    matrix = [list(map(int, input())) for _ in range(100)]
-    for i in range(100):
-        for j in range(100):
-            if matrix[i][j] == 2:
-                goal_y, goal_x = i, j
-    # 동 북 서
-    dx = [1, 0, -1]
-    dy = [0, -1, 0]
-    # 진행 방향
-    d = 1
-    dir = {
-        'E':0,
-        'U':1,
-        'W':2,
-    }
-    while True:
-        # 1. 앞으로 간다.
-        # 2. 오른쪽 혹은 왼쪽으로 갈 수 있는지 확인한다.
-        # 3. 갈 수 있다면 방향 전환을 한다.
-        # 4. 왼쪽으로 갔다면 진행 후 오른쪽으로 갈 수 있는지 확인한다.
-        # 5. 오른쪽으로 갔다면 진행 후 왼쪽으로 갈 수 있는지 확인한다.
-        # 6. 세 방향 모두 진행 불가능 하다면 이는 사다리의 시작지점이다.
-        ny = i + dy[d]
-        nx = j + dx[d]
+A, B, V = map(int, input().split())
 
+start = 0
+end = 1000000000 
+#t = 0  문제점 찾기용
+# 이진 탐색 이용
+# 1시간 반 소요...
+#지피티랑 대화 몇 번 했는데 할루시 개 당했습니다...
+while True:
+    # t += 1
+    i = (start + end) // 2 #중간 값
 
+    # V보다 큰 i의 최솟값을 찾는다.
+    # A*i -B*(i-1) >= V  --> 낮에 도달 시 즉시 종료이므로 B 값에 -1을 해준다.
+    if A*i -B*(i-1) >= V: # V보다 큰 i 를 찾는다.
+        i -= 1 # i에 1을 빼고
+        if A*i -B*(i-1) >= V: # 다시 계산해도 크다면 해당 i는 최솟값이 아니다.
+            end = i  # 위에서 -1을 했으므로 해당 i를 end로 바꾼다.
+            continue # 즉시 반복
+        else: # 만약 첫 i만이 V보다 크다는 것을 만족하면 해당 i는 v보다 크거나 같은 최솟값이 된다.
+            answer = i +1 #i에 1을 뺐으므로 더한다.
+            break
+    elif  A*i -B*(i-1) <= V: # 작은경우 시작 값을 키운다.
+        start = i + 1
+    # print(start, end, i)
+print(answer)
