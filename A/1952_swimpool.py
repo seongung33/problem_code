4c433bfe3fe85a1e943b930fb90a4f174095c15c
seongung33 <seongung33@gmail.com>
2026-03-03 17:21:34 +0900

    aa

---
+++ A/1952_swimpool.py
@@ -0,0 +1,60 @@
+T = int(input())
+for test in range(1, T+1):
+    day, month, three, year = map(int, input().split())
+    lst = list(map(int, input().split()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # pay = 0
+    # allday = sum(lst)
+    # cnt = 0
+    # for i in lst:
+    #     if i:
+    #         cnt += 1
+    # if month*12 > year and three*4 > year:
+    #     if allday* day > year:
+    #         pay += year
+    # day_pay_lst =[0]*12
+    # for i in lst:
+    #     day_pay_lst = i*day
+    # # 1달 이용권 표기
+    # month_pay_lst = [False] * 12
+    # # 3달 이용권 표기
+    # three_pay_lst = [False] * 12
+    # for i in range(12):
+    #     if day_pay_lst[i] > month:
+    #         month_pay_lst[i] = True
+    #
+    # for i in range(10):
+    #     if month_pay_lst[i] and month_pay_lst[i+1] and month_pay_lst[i+2]:
+    #         if month*3 > three:
+    #             three_pay_lst[i] = True
+    #
+    # if month_pay_lst
+    #
+    #
+    # for i in range
